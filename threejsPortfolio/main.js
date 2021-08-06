c8867984af4c6c08365fcb5ed41f9714bd1cccc3
davidwpacker <davidwgpacker@gmail.com>
2021-08-06 23:20:54 +1000

    initial setup

---
+++ threejsPortfolio/main.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene(); // scene == container 
+const camera = new.THREE.PerspectiveCamera(); // human perspective
